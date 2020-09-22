1e79221365d62005f419948c5ae9c059e93f3621
Lianyou Wang <wly1250623585@gmail.com>
2020-09-22 21:48:07 +0800

    basepagecomponent

--- src/components/base/BasePageComponent.tsx
+++ src/components/base/BasePageComponent.tsx
@@ -1,7 +1,8 @@
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
+import navigationService from '@router/navigaitonService';
 import { Stores } from '@store';
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { BackHandler, NativeEventSubscription, Platform, View } from 'react-native';
 
 
 
@@ -34,10 +35,151 @@ export default class BasePageComponent<P extends BaseComponentProps, S extends B
      */
     isDownRefresh: boolean = false
 
+
+    /**
+     *Android物理键返回时间监听
+     *
+     * @type {NativeEventSubscription}
+     * @memberof BasePageComponent
+     */
+    listenerAndroidBack: NativeEventSubscription
+
     constructor(props: P) {
         super(props)
         this.navigation = props.navigation;
+
+
+        this.loadData();
+        this.addViewLifeCircleEventListener()
+        this.addAndroidHardBackPressListener()
+
     };
+
+
+    componentWillUnmount() {
+        this.removeLifeCircleEventListener()
+        this.removeAndroidHardBackPressListener()
+    }
+
+
+    addAndroidHardBackPressListener() {
+
+        if (this.navigation && this.navigation.isFocused()) {
+            if (Platform.OS == "android") {
+                this.listenerAndroidBack = BackHandler.addEventListener("hardwareBackPress", () => {
+                    this.onBackButtonPressAndroid();
+                    return true;
+                });
+            }
+        }
+    }
+
+    removeAndroidHardBackPressListener() {
+        if (this.navigation && this.navigation.isFocused()) {
+            if (Platform.OS == "android") {
+                this.listenerAndroidBack && this.listenerAndroidBack.remove()
+            }
+        }
+    }
+
+    /**
+     * 默认加载数据
+     */
+    loadData() {
+        console.log('BasePageComponent ---- loadData');
+    }
+
+    /**
+     * 加载更多数据
+     */
+    loadMoreData() {
+        console.log('BasePageComponent --- loadMoreData');
+        this.page_index++
+        this.isDownRefresh = false
+
+    }
+
+    /**
+     * 加载最新数据
+     */
+    loadLastedData() {
+        console.log('BasePageComponent ----- loadLastedData');
+        this.page_index = 1
+        this.isDownRefresh = true
+
+    }
+
+
+    /**
+    * 添加component的生命周期listener（跟navigation无关联）
+    */
+    addViewLifeCircleEventListener() {
+        this.navigation.addListener("focus", (payload) => {
+            console.log('focus', payload);
+            this.componentAppear()
+
+        })
+        this.navigation.addListener("blur", (payload) => {
+            console.log('blur', payload);
+            this.componentDisappear()
+
+        })
+    }
+    /**
+     * 移除component的生命周期listener（跟navigation无关联）
+     */
+    removeLifeCircleEventListener() {
+        this.navigation.removeListener("focus", (payload) => { })
+        this.navigation.removeListener("blur", (payload) => { })
+
+    }
+
+    componentAppear() {
+
+    }
+    componentDisappear() {
+
+    }
+
+
+    /**
+    * 安卓物理按键返回
+    */
+    onBackButtonPressAndroid() {
+        let { navigation } = this.props;
+
+        if (navigation) {// 必须在有navigation的情况下才能操作界面、
+
+            let isCurrentPageFocus = navigation.isFocused()
+
+            if (isCurrentPageFocus) {
+                if (this.onNavigationBackPress) {
+                    this.onNavigationBackPress();
+                    return true
+                } else {
+                    return false
+                }
+            }
+            else {
+                navigationService.goBack();
+            }
+
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 返回键默认实现 【导航栏返回键、安卓物理返回键】
+     */
+    onNavigationBackPress() {
+        navigationService.goBack()
+
+    }
+
+
+
 
 }
 
